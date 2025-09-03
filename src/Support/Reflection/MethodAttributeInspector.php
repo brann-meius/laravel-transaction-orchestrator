5c514267a3bce16b9c7a146af488650905540961
brann-meius <bohdan.meius@gmail.com>
2025-09-03 12:18:20 +0200

    fixed: replaced new static() with new (static::class)

--- src/Support/Reflection/MethodAttributeInspector.php
+++ src/Support/Reflection/MethodAttributeInspector.php
@@ -11,14 +11,14 @@ class MethodAttributeInspector extends AttributeInspector
     public static function for(?string $controller = null, ?string $method  = null): static
     {
         if (is_null($controller) || !class_exists($controller)) {
-            return new static(null);
+            return new (static::class)(null);
         }
 
         if (is_null($method) || !method_exists($controller, $method)) {
-            return new static(null);
+            return new (static::class)(null);
         }
 
-        return new static(
+        return new (static::class)(
             new ReflectionMethod($controller, $method)
         );
     }
